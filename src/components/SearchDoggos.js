4e37320e401d888e4e5133c7734b543ab65da59a
PeterArriaza <peter.arriaza@gmail.com>
2020-03-08 21:53:31 -0400

    added api call to fill list of breeds

--- src/components/SearchDoggos.js
+++ src/components/SearchDoggos.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/SearchDoggos.css";
 
-export default function SearchDoggos() {
-  return (
-    <div>
-      <p>SearchDoggos</p>
-      <Link to="/">
-        <button>Cancel</button>
-      </Link>
-    </div>
-  );
+export default class SearchDoggos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breedList: null
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+      .then(res => (res.ok ? res : Promise.reject(res)))
+      .then(res => res.json())
+      .then(data => this.setState({ breedList: data.message }));
+  }
+
+  render() {
+    console.log("breeds are", this.state.breedList);
+    let breeds;
+    this.state.breedList === null
+      ? (breeds = "")
+      : (breeds = Object.keys(this.state.breedList).map((breed, index) => (
+          <option value={breed} key={index}>
+            {breed}
+          </option>
+        )));
+
+    return (
+      <div>
+        <p>SearchDoggos</p>
+        <form>
+          <label>Pick a breed:</label>
+          <select id="breeds">{breeds}</select>
+        </form>
+        <Link to="/">
+          <button>Cancel</button>
+        </Link>
+      </div>
+    );
+  }
 }
